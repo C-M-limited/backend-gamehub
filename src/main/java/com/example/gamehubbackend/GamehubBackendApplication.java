f8c8e9c7152458986c71bb0a07d9cd32bd480522
marttsang2 <=>
2022-01-31 17:43:30 +0800

    java spring setup

---
+++ src/main/java/com/example/gamehubbackend/GamehubBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.gamehubbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamehubBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamehubBackendApplication.class, args);
+	}
+
+}
