f84fe191162d37838c983cca368a12615a2b07ac
Chris <chrisleebed@gmail.com>
2023-10-19 11:03:29 -0700

    add cors

--- src/main/java/com/example/gamehubbackend/config/cors/CorsConfig.java
+++ src/main/java/com/example/gamehubbackend/config/cors/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry){
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000","https://www.gamehub.link/")
+                        .allowedOrigins("http://localhost:3000","https://www.gamehub.link/", "https://stupendous-stroopwafel-452aba.netlify.app/")
                         .allowedMethods("*");
             }
         };
